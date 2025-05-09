193cfd4712a08390bdd9decd2f4e04877ce6fd66
jessK10 <163406443+jessK10@users.noreply.github.com>
2025-05-10 01:28:27 +0200

    Initial commit for frontend

--- ecommerce-frontend/src/main.jsx
+++ ecommerce-frontend/src/main.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
-import "bootstrap/dist/css/bootstrap.min.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
 import "bootstrap-icons/font/bootstrap-icons.css";
 import "./index.css";
 
